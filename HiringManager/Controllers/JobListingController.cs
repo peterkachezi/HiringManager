53cea55084ebcde98b0a3b9d48b6b0b42aa37103
Kachezi <Kachezi@DESKTOP-B695AI8>
2022-06-20 12:15:36 +0300

    new data

---
+++ HiringManager/Controllers/JobListingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HiringManager.Controllers
+{
+
+    public class JobListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
